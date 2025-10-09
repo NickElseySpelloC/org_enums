94a0a72ac63ad15702e93e2964888d2d03829ee8
Nick Elsey <nick@spelloconsulting.com>
2025-10-09 17:40:12 +1100

    Additions to StateReasonOn and StateReasonOff

--- org_enums/enum_power_controller.py
+++ org_enums/enum_power_controller.py
@@ -14,14 +14,14 @@ __all__ = [
 
 
 class AppMode(StrEnum):
-    """Override modes for the mobile app"""
+    """Override modes for the mobile app."""
     ON = "on"
     OFF = "off"
     AUTO = "auto"
 
 
 class SystemState(StrEnum):
-    """Enumerate the overall system state"""
+    """Enumerate the overall system state."""
     DATE_OFF = "DateOff condition met for today"
     INPUT_OVERRIDE = "Input has overridden the mode"
     APP_OVERRIDE = "App has overridden the mode"
@@ -29,19 +29,19 @@ class SystemState(StrEnum):
 
 
 class RunPlanMode(StrEnum):
-    """Mode for creating run plans"""
+    """Mode for creating run plans."""
     BEST_PRICE = "BestPrice"
     SCHEDULE = "Schedule"
 
 
 class RunPlanTargetHours(StrEnum):
-    """Mode for run plan target hours"""
+    """Mode for run plan target hours."""
     NORMAL = "run for target hours"
     ALL_HOURS = "all available hours"
 
 
 class RunPlanStatus(StrEnum):
-    """Status of the run plan creation"""
+    """Status of the run plan creation."""
     NOTHING = "The required_hours were zero, so the run plan is empty."
     FAILED = "Unable to create the run plan. Could not allocate all required priority hours."
     PARTIAL = "The run plan was only partially filled, but the priority hours were allocated."
@@ -49,14 +49,15 @@ class RunPlanStatus(StrEnum):
 
 
 class StateReasonOn(StrEnum):
-    """Enumerate the reasons why the Output is on"""
+    """Enumerate the reasons why the Output is on."""
     APP_MODE_ON = "App has overridden the mode to on"
     INPUT_SWITCH_ON = "Device input has overridden the mode to on"
     ACTIVE_RUN_PLAN = "Run plan dictates that the output should be on"
+    MIN_ON_TIME = "Minimum on time has not yet elapsed"
 
 
 class StateReasonOff(StrEnum):
-    """Enumerate the reasons why the Output is off"""
+    """Enumerate the reasons why the Output is off."""
     NO_RUN_PLAN = "No run plan available"
     RUN_PLAN_COMPLETE = "No more run time required today"
     INACTIVE_RUN_PLAN = "Run plan dictates that the output should be off"
@@ -68,3 +69,4 @@ class StateReasonOff(StrEnum):
     DAY_END = "A new day has started"
     SHUTDOWN = "System is shutting down"
     DEVICE_OFFLINE = "Device is offline"
+    MIN_OFF_TIME = "Minimum off time has not yet elapsed"
